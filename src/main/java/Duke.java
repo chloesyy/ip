6250f9b50358cc09696d2d3ee2c045e487b7d5ab
chloesyy <chlyiyi@hotmail.com>
2020-08-22 17:34:06 +0800

    Level-1: Add echo and exit features to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Hello! I'm Duke.");
         System.out.println("What can I do for you?");
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(line);
+            line = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
